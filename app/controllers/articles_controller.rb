51e7018aa9b434a54e82f5b18a211bedca9cf413
LEVII\Admin <luongbc256@gmail.com>
2017-10-09 00:20:55 +0700

    [TMDT-Lab01] - update layout, articles view, add detail article page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,8 +11,17 @@ class ArticlesController < ApplicationController
        end
     end
 
+    def show
+        @article = Article.find(params[:id])
+    end
+
     def create
         @article = User.find(session[:user_id]).articles.create(:title => params[:title], :content => params[:content], :view => '0', :avatar => 'http://www.iconsfind.com/wp-content/uploads/2015/08/20150831_55e46b1f4a412.png')
         redirect_to '/articles'
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        @article.update(view: params[:view]) 
     end
 end
